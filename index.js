06e46c12d4d52e6e4c3ed2ef56414318a0135a47
Paul Jung <pbnjcub@gmail.com>
2022-06-16 19:19:12 -0400

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const main = document.getElementById('main');
+main.remove();
+
+const newHeader = document.getElementById('victory');
+newHeader.textContent = "PAUL JUNG is the champion";
